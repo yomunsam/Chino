3a62f9cc3c5d0ef4fb5159dfeef7bb705b78eb5b
大魔王有木桑 <yomunsam@outlook.com>
2020-12-02 17:42:45 +0800

    “是否开放注册新用户”的开关在关闭时，Register页面直接不让访问

--- src/Chino.IdentityServer/Pages/Account/Register.cshtml.cs
+++ src/Chino.IdentityServer/Pages/Account/Register.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Chino.IdentityServer.Configures;
 using Chino.IdentityServer.Dtos.Account;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,12 +11,40 @@ namespace Chino.IdentityServer.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        private readonly ChinoAccountConfiguration m_AccountConfiguration;
+
+        public RegisterModel(ChinoAccountConfiguration chinoAccountConfiguration)
+        {
+            m_AccountConfiguration = chinoAccountConfiguration;
+        }
+
         [BindProperty]
         public RegisterPageDto RegisterDto { get; set; }
 
-        public void OnGet()
+        public IActionResult OnGetAsync()
         {
+            
+            if (!m_AccountConfiguration.EnableRegister)
+            {
+                //TODO: 等做好错误页之后，这里最好是跳转到错误页面
+                return Redirect("/");
+            }
+
+            return Page();
         }
+
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await Task.Yield();
+
+            return Page();
+        }
+
+
+
+
 
 
 
